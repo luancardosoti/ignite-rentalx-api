44e4270b4d7f766dc2d556978e96f5057f4646fb
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2022-04-10 19:18:07 -0300

    criando migration de especificação

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -4,14 +4,14 @@ import { container } from "tsyringe";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response) {
     const { name, description } = request.body;
 
-    const createScpecificationUseCase = container.resolve(
+    const createSpecificationUseCase = container.resolve(
       CreateSpecificationUseCase
     );
 
-    createScpecificationUseCase.execute({ name, description });
+    await createSpecificationUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
