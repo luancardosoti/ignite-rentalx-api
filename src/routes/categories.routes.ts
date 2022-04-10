713d50c00fcd1c1a9b34a2812089c145d965f3c3
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2022-04-10 12:16:25 -0300

    refatorando dependencias de createCategory com Tsyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import multer from "multer";
 
-import createCategoryController from "../modules/cars/useCases/createCategory";
+// import createCategoryController from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const categoriesRoutes = Router();
 
 const upload = multer({
   dest: "./tmp",
 });
 
-const categoriesRoutes = Router();
+const createCategoryController = new CreateCategoryController();
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
