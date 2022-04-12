38837af37a05b2a64ebc9b23ff717a05c6050d25
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2022-04-11 23:22:26 -0300

    salvando usuário no banco

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./CreateUserUserCase";
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const createUserCase = container.resolve(CreateUserUseCase);
+
+    const { name, username, email, password, driver_license } = request.body;
+
+    await createUserCase.execute({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+
+    return response.status(201).send();
+  }
+}
+
+export { CreateUserController };
--- src/server.ts
+++ src/server.ts
@@ -17,4 +17,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(4000, () => console.log("Server is running."));
+app.listen(4000, () => console.log("Server is running on port 4000"));
