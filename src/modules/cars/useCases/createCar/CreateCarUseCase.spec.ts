f743475c89a282c43408452c33e636e710d3ac1b
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2022-04-18 22:37:40 -0300

    finalizando caso de uso createCar com dados em memória

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/inMemory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
@@ -13,7 +14,7 @@ describe("Create Car", () => {
   });
 
   it("should be able to create a new car", async () => {
-    await createCarUserCase.execute({
+    const car = await createCarUserCase.execute({
       name: "Fusca",
       description: "Carro de luxo",
       daily_rate: 100,
@@ -22,5 +23,46 @@ describe("Create Car", () => {
       brand: "VW",
       category_id: 1,
     });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a car wiith exists license plate", async () => {
+    expect(async () => {
+      const license_plate = "ABC-1234";
+      await createCarUserCase.execute({
+        name: "Car 1",
+        description: "Carro de luxo",
+        daily_rate: 100,
+        license_plate,
+        fine_amount: 10,
+        brand: "VW",
+        category_id: 1,
+      });
+
+      await createCarUserCase.execute({
+        name: "Car 2",
+        description: "Carro de luxo",
+        daily_rate: 100,
+        license_plate,
+        fine_amount: 10,
+        brand: "VW",
+        category_id: 1,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a car with available true by default ", async () => {
+    const car = await createCarUserCase.execute({
+      name: "Fusca",
+      description: "Carro de luxo",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 10,
+      brand: "VW",
+      category_id: 1,
+    });
+
+    expect(car.available).toBe(true);
   });
 });
